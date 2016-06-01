fb27321d7a974c7678edc3ded63c1beb96a33b2b
Ian Grosh <iangrosh@outlook.com>
2016-05-31 22:05:32 -0500

    first commit just the readmen and the using statments

---
+++ HelloWorld.pl
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+use diagnostics;
+# say prints a line followed by a newline
+use feature 'say';
+# Use a Perl version of switch called given when
+use feature "switch";
+# Use Perl version 5.14
+use v5.14;
+
---
+++ README.txt
@@ -0,0 +1,4 @@
+README!
+This is my learning of Perl 
+I started from the Perl Tutorial by Derek Banas 
+http://www.newthinktank.com/2016/03/perl-tutorial/
